2c18dd93e211bf465c3724c47308c13449581d77
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-04-24 16:00:05 +0200

    Do work on the database controller

--- db.sql
+++ db.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS questions (
 	id INTEGER PRIMARY KEY NOT NULL,
 	quiz_id INTEGER NOT NULL,
 	text TEXT NOT NULL,
+	type VARCHAR NOT NULL,
 
 	FOREIGN KEY(quiz_id) REFERENCES quiz(id)
 );
@@ -15,6 +16,7 @@ CREATE TABLE IF NOT EXISTS answers (
 	id INTEGER PRIMARY KEY,
 	question_id INT NOT NULL,
 	text TEXT NULL,
+	is_right BOOL NOT NULL DEFAULT false,
 	
 	FOREIGN KEY(question_id) REFERENCES questions(id)
 );
--- quiz/db/__init__.py
+++ quiz/db/__init__.py
@@ -3,67 +3,74 @@ import sqlite3
 
 def make_database(db_name="quizzes.db", schema="db.sql"):
     conn = sqlite3.connect(db_name, isolation_level=None)
+    
     cur = conn.cursor()
     with open(schema, "r") as f:
         cur.executescript(f.read())
 
     cur.close()
+
     return conn
 
 db = make_database()
 
-def get_question(num):
+def query(q, ret=None, args=tuple()):
     cur = db.cursor()
-    cur.execute("SELECT text FROM questions WHERE id = ?", (num,))
-    data = cur.fetchone()
+    cur.execute(q, args)
     cur.close()
 
-    return data[0]
+    if not ret:
+        return
+
+    if ret == 'insert':
+        return cur.lastrowid
+
+    if ret == 'fetch_one':
+        return cur.fetchone()
+
+    if ret == 'fetch_all':
+        return cur.fetchall()
+
+    else:
+        raise ValueError('Invalid query return type.')
+
+
+def get_all_quizes():
+    return query("SELECT name FROM quizzes", "fetch_all")
+
+
+def get_quiz_questions(quiz_id):
+    return query("SELECT id FROM questions WHERE quiz_id = ?", "fetch_all", (quiz_id,))
+
+
+def get_question(num):
+    return query("SELECT text FROM questions WHERE id = ?", "fetchone", (num,))
 
 
 def get_answers(num):
-    cur = db.cursor()
-    cur.execute("SELECT text FROM answers WHERE id = ?", (num,))
-    data = cur.fetchall()
-    cur.close()
-
-    return data[0]
+    return query("SELECT text, is_right  FROM answers WHERE id = ?", "fetchall", (num,))
 
 
 def new_quiz(name):
-    cur = db.cursor()
-    cur.execute("INSERT INTO quizzes(name) VALUES (?)", (name,));
-    quiz_id = cur.lastrowid
-    cur.close()
-
-    return quiz_id
+    return query("INSERT INTO quizzes(name) VALUES (?)", "insert", (name,))
 
 
-def new_question(quiz_id, question):
-    cur = db.cursor()
-    cur.execute("INSERT INTO questions(quiz_id, text) VALUES (?, ?)", (quiz_id, question,))
-    question_id = cur.lastrowid
-    cur.close()
-
-    return question_id
+def new_question(quiz_id, question, _type):
+    return query("INSERT INTO questions(quiz_id, text, type) VALUES (?, ?, ?)", "insert", (quiz_id, question, _type))
 
 
-def new_answer(question_id, answer):
-    cur = db.cursor()
-    cur.execute("INSERT INTO answers(question_id, text) VALUES (?, ?)", (question_id, answer,))
-    answer_id = cur.lastrowid
-    cur.close()
+def new_answer(question_id, answer, is_right):
+    return query("INSERT INTO answers(question_id, text, is_right) VALUES (?, ?, ?)", "insert", (question_id, answer, is_right))
 
-    return answer_id
 
 if __name__ == "__main__":
     quiz_id = new_quiz("Maths quiz")
     print(f"{quiz_id=}")
-    question_id = new_question(quiz_id, "What is 2+2?")
+    question_id = new_question(quiz_id, "What is 2+2?", "multiple choice")
     print(f"{question_id=}")
     ids = []
     for i in range(1, 5):
-        ids.append(new_answer(question_id, str(i)))
+        ids.append(new_answer(question_id, str(i), True if i == 4 else False))
 
     print(f"{ids=}")
     db.close()
