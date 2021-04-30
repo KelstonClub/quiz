55dee4259ddde64837c531b8ee6f3bc9a2b2bc1e
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-04-30 21:32:32 +0200

    Make some changes to the startup shell script

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 
 python -mvenv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
-python load_into_database.py data.csv
+pip install -r requirements.txt &>/dev/null
+if [ ! -f quizzes.db ]; then
+	python load_into_database.py data.csv
+fi
 python -mquiz
