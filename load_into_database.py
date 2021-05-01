42b10581191c841152e934ac3db19610b7523373
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-05-01 16:12:18 +0200

    Fix load_into_database.py file

--- load_into_database.py
+++ load_into_database.py
@@ -1,60 +1,43 @@
 import sys
 import csv
-from quiz import db
+from quiz.db import Database
 
-
-ACTIONS = {
-    "quizzes": db.new_quiz,
-    "questions": db.new_question,
-    "answers": db.new_answer,
-}
-
-REL_ID = {
-    "questions": "quizzes",
-    "answers": "questions"
-}
-
-
-def rewrite_args(token, ids, *args):
-    if token == "quizzes":
-        return args[0]
-
-    args = list(args)
-    args[0] = ids[REL_ID[token]][int(args[0]) - 1]
-
-    return args
-
-
-def parse(conn, csv_file):
+def parse(db, csv_file):
     ids = {"quizzes": [], "questions": []}
     
     for line in csv_file:
         token, *args = line
-        if not token in ACTIONS:
-            raise ValueError(f"Invalid element of type=`{token}`")
-
-        try:
-            args = rewrite_args(token, ids, *args)
-            rval = ACTIONS[token](conn, args)
+        print(token, args)
+        if token == "quizzes":
+            rval = db.conn.execute("INSERT INTO quizzes(name) VALUES (?)", args).lastrowid
         
-        except Exception as e:
-            print(f"Catched exception while inserting element of type=`{token}` into database with arguments=`{list(args)}`. This was possible caused because of an invalid formatted data file.\nError: {e}\n")
+        elif token == "questions":
+            qid = ids["quizzes"][int(args[0]) - 1]
+            rval = db.conn.execute("insert into questions(quiz_id, text, type) VALUES (?, ?, ?)", (qid, args[1], args[2])).lastrowid
 
-        finally:
-            print(f"Inserted element in table `{token}`.")
-            if token in ids.keys():
-                ids[token].append(rval)
+        elif token == "answers":
+            qid = ids["questions"][int(args[0]) - 1]
+            rval = db.conn.execute("insert into answers(question_id, text, is_right) VALUES (?, ?, ?)", (qid, args[1], args[2]))
 
+        else:
+            raise ValueError(f"Invalid token: `{token=}`")
+
+        if token in ["quizzes", "questions"]:
+            ids[token].append(rval)
+
+
+
+class App:
+    config = {"DATABASE_PATH": "quizzes.db"}
 
 if __name__ == "__main__":    
     if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} <csv file>")
         sys.exit()
 
-
     with open(sys.argv[1], "r") as f:
         csv_file = csv.reader(f)
-        conn = db.make_db()
-        parse(conn, csv_file)
+        db = Database(App)
+        parse(db, csv_file)
 
     print("Finished inserting data.")
