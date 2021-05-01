a10dfa14c66df9a2b4dd6dda164f071a2dd3b8e7
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-05-01 18:28:10 +0200

    Add a semi working version of the scraper and its data.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
+import re
 
 BASE_SITE = "https://www.jetpunk.com"
 BASE_URL = "/tags/multiple-choice"
@@ -33,27 +34,59 @@ def get_quiz_questions_and_possible_answers(content):
 
 def get_quiz_answers(content):
     soup = BeautifulSoup(content, "html.parser")
-    data = soup.find(class_="post-game-stat-table")
-    print(data)
-    return
+    data = soup.find(class_="post-game-stat-table").find_all('td')
+    q_and_a = [i.text for i in data if not "\n" in i.text]
+    return q_and_a
+
+
+def into_csv_format(qid, data):
+    fmt = f"quizzes, {data[0]}"
+    for i, (question, answers) in enumerate(data[1].items(), 1):
+        fmt += f"\nquestions,{qid},{question},multiple choice"
+        for answer in answers:
+            fmt += f"\nanswers,{i},{answer},False"
+
+    for q, a in zip(data[2], data[2][1::]):
+        qid = re.search(f"questions,([0-9]*),{q},multiple choice", fmt)
+        # For some reason some questions are not found
+        if qid:
+            qid = qid.group(1)
+        else:
+            qid = '1'
+
+        fmt.replace(f"{qid},{a},False", f"{qid},{a},True")
+
+    return fmt
 
 
 if __name__ == "__main__":
     page = requests.get(BASE_SITE + BASE_URL)
     soup = BeautifulSoup(page.content, "html.parser")
     links = soup.find_all("a", href=True)
-    quizzes = []
+    quizzes = set()
     for link in links:
         if link["href"].startswith("/quizzes/") and link["href"]:
             url = urljoin(link["href"], urlparse(link["href"]).path)
             if url != "/quizzes/random":
-                quizzes.append(BASE_SITE + url)
+                quizzes.add(BASE_SITE + url)
 
     data = []
-    for quiz in quizzes:
+    for i, quiz in enumerate(quizzes, 1):
         qhtml = requests.get(quiz).content
         qname = get_quiz_name(qhtml)
         questions = get_quiz_questions_and_possible_answers(qhtml)
+        if not questions:
+            continue
+        
         ahtml = requests.get(quiz + "/stats").content
         answers = get_quiz_answers(ahtml)
-        print(quiz, " => ", [qname, questions, answers])
+        if not answers:
+            continue
+        
+        data = (qname, questions, answers)
+        formatted = into_csv_format(i, data)
+
+        print(f"Finished loading quiz {i}")
+        with open('jetpunk.csv', 'a') as f:
+            f.write(formatted)
+
